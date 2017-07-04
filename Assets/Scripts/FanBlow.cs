a81085d23e0a6aae2cfc80edbd0902bf5f159caf
Gabriel de Diego <gdedi651@gmail.com>
2017-07-03 20:16:59 -0400

    feat: add bounce affect to trampoline and ball

---
+++ Assets/Scripts/FanBlow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FanBlow : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerStay(Collider col) {
+        Debug.Log(col.gameObject.name);
+    }
+}

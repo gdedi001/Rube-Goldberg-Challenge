d02cd7e5ce5ef36a04a53e9815da59e53f0dcc7d
Gabriel de Diego <gdedi651@gmail.com>
2017-08-10 18:53:48 -0400

    feat: add floating orb as button to remove instructions

---
+++ Assets/Scripts/NotificationManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotificationManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        PlayAudio();
+	}
+	
+    void PlayAudio() {
+        gameObject.GetComponent<AudioSource>().Play();
+    }
+}
